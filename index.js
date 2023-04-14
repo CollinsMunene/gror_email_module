de16b322853c7b27df8203a7ae3414faefa97ed5
Collins Hillary <collinshillary@Collinss-MacBook-Air.local>
2023-04-14 09:48:50 +0300

    add pdf required argument

--- index.js
+++ index.js
@@ -44,101 +44,140 @@ function sendEmailWithPdf(
   email_subject,
   email_text,
   recipients,
-  zip_required
+  zip_required,
+  pdf_required
 ) {
   if (transport) {
-    const output = fs.createWriteStream(
-      __dirname + "/" + email_subject + ".zip"
-    );
-    pdf(html).then(async (pdfBuffer) => {
-      if (pdfBuffer != false) {
-        if (zip_required) {
-          let archive = archiver("zip", {
-            zlib: { level: 9 }, // compression level
-          });
+    if (pdf_required) {
+      //generate pdf
+      const output = fs.createWriteStream(
+        __dirname + "/" + email_subject + ".zip"
+      );
+      pdf(html).then(async (pdfBuffer) => {
+        if (pdfBuffer != false) {
+          if (zip_required) {
+            let archive = archiver("zip", {
+              zlib: { level: 9 }, // compression level
+            });
 
-          archive.pipe(output);
+            archive.pipe(output);
 
-          archive.append(pdfBuffer, { name: file_name });
+            archive.append(pdfBuffer, { name: file_name });
 
-          archive.finalize();
+            archive.finalize();
 
+            var GeneralHelperOptions = {
+              from: "grorapp@gror.io",
+              to: recipients,
+              subject: email_subject,
+              html: email_text,
+              attachments: output,
+            };
+          } else {
+            var GeneralHelperOptions = {
+              from: "grorapp@gror.io",
+              to: recipients,
+              subject: email_subject,
+              html: email_text,
+              attachments: output,
+            };
+          }
+          try {
+            transport.sendMail(GeneralHelperOptions, (error, info) => {
+              if (error) {
+                console.log(error);
+                logger.error(error.message, {
+                  path: "",
+                  function: `${file_name} Email Process`,
+                });
+              } else {
+                console.log("Email Sent");
 
-          var GeneralHelperOptions = {
-            from: "grorapp@gror.io",
-            to: recipients,
-            subject: email_subject,
-            html: email_text,
-            attachments: output,
-          };
+                logger.info(`Email sent successfully. ${info.messageId}`, {
+                  path: "",
+                  function: `${file_name} Email Process`,
+                });
+              }
+            });
+          } catch (error) {
+            console.log(error);
+            logger.error(error.message, {
+              path: "module",
+              function: `${file_name} Email Process`,
+            });
+          }
         } else {
-          var GeneralHelperOptions = {
+          console.log(
+            "Attachment is too big, sending email without attachment"
+          );
+          var zGeneralHelperOptions = {
             from: "grorapp@gror.io",
             to: recipients,
             subject: email_subject,
             html: email_text,
-            attachments: output,
           };
-        }
-        try {
-          transport.sendMail(GeneralHelperOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-              logger.error(error.message, {
-                path: "",
-                function: `${file_name} Email Process`,
-              });
-            } else {
-              console.log("Email Sent");
+          try {
+            transport.sendMail(zGeneralHelperOptions, (error, info) => {
+              if (error) {
+                console.log(error);
+                logger.error(error.message, {
+                  path: "",
+                  function: `${file_name} Email Process`,
+                });
+              } else {
+                console.log("Email sent");
 
-              logger.info(`Email sent successfully. ${info.messageId}`, {
-                path: "",
-                function: `${file_name} Email Process`,
-              });
-            }
-          });
-        } catch (error) {
-          console.log(error);
-          logger.error(error.message, {
-            path: "module",
-            function: `${file_name} Email Process`,
-          });
+                logger.info(`Email sent successfully. ${info.messageId}`, {
+                  path: "",
+                  function: `${file_name} Email Process`,
+                });
+              }
+            });
+          } catch (error) {
+            console.log(error);
+            //send email with text only
+            logger.error(error.message, {
+              path: "module",
+              function: `${file_name} Email Process`,
+            });
+          }
         }
-      } else {
-        console.log("Attachment is too big, sending email without attachment");
-        var zGeneralHelperOptions = {
-          from: "grorapp@gror.io",
-          to: recipients,
-          subject: email_subject,
-          html: email_text,
-        };
-        try {
-          transport.sendMail(zGeneralHelperOptions, (error, info) => {
-            if (error) {
-              console.log(error);
-              logger.error(error.message, {
-                path: "",
-                function: `${file_name} Email Process`,
-              });
-            } else {
-              console.log("Email sent");
+      });
+    } else {
+      //don't generate pdf
 
-              logger.info(`Email sent successfully. ${info.messageId}`, {
-                path: "",
-                function: `${file_name} Email Process`,
-              });
-            }
-          });
-        } catch (error) {
-          console.log(error);
-          //send email with text only
-          logger.error(error.message, {
-            path: "module",
-            function: `${file_name} Email Process`,
-          });
-        }
+      var GeneralHelperOptions = {
+        from: "grorapp@gror.io",
+        to: recipients,
+        subject: email_subject,
+        html: email_text,
+      };
+
+      try {
+        transport.sendMail(GeneralHelperOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            logger.error(error.message, {
+              path: "",
+              function: `${file_name} Email Process`,
+            });
+          } else {
+            console.log("Email Sent");
+
+            logger.info(`Email sent successfully. ${info.messageId}`, {
+              path: "",
+              function: `${file_name} Email Process`,
+            });
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        logger.error(error.message, {
+          path: "module",
+          function: `${file_name} Email Process`,
+        });
       }
-    });
+    }
   } else {
     console.log("Kindly initialize the package");
     logger.error("Kindly initialize the package", {
